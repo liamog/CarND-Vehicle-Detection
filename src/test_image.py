c4d4b066f7411ccdc8257c0eb0f745f1b6585cb4
Liam O'Gorman <liam@liamogorman.com>
2017-12-08 08:56:55 -0800

    separate dector per image to avoid heatmap confusion

--- src/test_image.py
+++ src/test_image.py
@@ -11,13 +11,15 @@ from vehicle_detector import VehicleDetector
 
 files = glob.glob('test_images/*.jpg')
 files.sort()
-detector = VehicleDetector()
 pathlib.Path("output_images").mkdir(parents=True, exist_ok=True)
 
 for file in files:
+    # New dector for each test image as we don't want our heatmap to
+    # cause weirdness over different frames.
+    detector = VehicleDetector()
     print(file)
     name, ext = os.path.splitext(os.path.basename(file))
     target = os.path.join('output_images', name + '_detected.jpg')
     img_rgb = cv2.imread(file)
-    final = detector.process_image(img_rgb)
+    final = detector.process_image_with_diagnostics(img_rgb)
     cv2.imwrite(target, final)
